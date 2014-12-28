b3eeb4aa386b5880ae84b5abffbb45b4f613126e
0xack13 <0xack13@gmail.com>
2014-12-28 21:15:37 +0300

    Add base and derived classes

--- NewVSOverride/Program.cs
+++ NewVSOverride/Program.cs
@@ -6,10 +6,27 @@ using System.Threading.Tasks;
 
 namespace NewVSOverride
 {
+    public class Base
+    {
+        public virtual void SomeOtherMethod()
+        {
+            Console.WriteLine("Base was called!");
+        }
+    }
+
+    public class Derived : Base
+    {
+        public new void SomeOtherMethod()
+        {
+            Console.WriteLine("Dervided was called!");
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
